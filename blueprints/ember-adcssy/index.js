9656e0111d60809646f3f5f2849b018b70c55f84
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-08-20 11:23:58 +0200

    Add normalizeEntityName method

--- blueprints/ember-adcssy/index.js
+++ blueprints/ember-adcssy/index.js
@@ -1,6 +1,8 @@
 /* global module */
 
 module.exports = {
+  normalizeEntityName: function() { },
+
   afterInstall: function() {
     return this.addBowerPackageToProject('adcssy', 'adfinis-sygroup/adcssy#master')
   }
