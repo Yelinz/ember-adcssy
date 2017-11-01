1f714d995aae26aa1e3f87904f98c10ac8261d8d
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-01 13:18:04 +0100

    Export adcssy tree instead of compiled

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
 /* global module, require, __dirname */
 
-const postcss = require('broccoli-postcss-single')
 const Funnel = require('broccoli-funnel')
-const mergeTrees = require('broccoli-merge-trees')
+const Merge = require('broccoli-merge-trees')
 
 const fs = require('fs')
 const path = require('path')
 
 const adcssyPath = path.dirname(require.resolve('adcssy/package.json'))
-const faPath = path.dirname(require.resolve('font-awesome/package.json'))
 
 module.exports = {
   name: 'ember-adcssy',
 
-  treeForPublic() {
+  treeForPublic(inputTree) {
     let trees = []
 
     let adcssyAssets = new Funnel(`${adcssyPath}/assets`, {
       destDir: '/'
     })
 
-    let faFonts = new Funnel(`${faPath}/fonts`, { destDir: '/fonts' })
-
     trees.push(adcssyAssets)
-    trees.push(faFonts)
 
-    return mergeTrees(trees, { overwrite: true })
+    return new Merge(trees)
   },
 
-  treeForAddon() {
-    return postcss([`${adcssyPath}/css`], 'adcssy.css', 'adcssy.css', {
-      plugins: [
-        { module: require('postcss-import') },
-        {
-          module: require('postcss-cssnext'),
-          options: {
-            browsers: '> 2%, last 2 versions, Firefox ESR',
-            features: { rem: false }
-          }
-        },
-        { module: require('postcss-responsive-type') }
-      ],
-      map: { inline: true }
-    })
+  treeForAddon(inputTree) {
+    return new Funnel(`${adcssyPath}/css`)
   }
 }
